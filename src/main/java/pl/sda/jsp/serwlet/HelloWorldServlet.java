e46d40e62193cee573e559be0aeaf76b24a0e93e
Jaśkiewicz <m.z.jaskiewicz@gmail.com>
2020-03-07 10:40:37 +0100

    zadanie 22

--- src/main/java/pl/sda/jsp/serwlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/jsp/serwlet/HelloWorldServlet.java
@@ -1,5 +1,7 @@
 package pl.sda.jsp.serwlet;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,12 +9,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
+
+    private String userId;
+    private String localization;
+
+    @Override
+    public void init(ServletConfig config) {
+        userId = config.getInitParameter("userId");
+        localization = config.getInitParameter("localization");
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
         writer.println("Hello world!");
+        writer.println("userId: " + userId);
+        writer.println("localization: " + localization);
     }
 }
